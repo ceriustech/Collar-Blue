b14cf30442b5207cb7253241f122b6490bc3948c
Bill <ceriustech@gmail.com>
2021-04-10 22:17:45 -0500

    Created a route for authentication.

--- api/routes/api/auth.js
+++ api/routes/api/auth.js
@@ -1,1 +1,14 @@
 // Handles getting JWT for authentication
+
+// Handles creating auth
+
+const express = require("express");
+const router = express.Router();
+
+// @route GET api/posts
+// @desc Test route
+// @access Public
+
+router.get("/", (req, res) => res.send("AUTH ROUTE!"));
+
+module.exports = router;
