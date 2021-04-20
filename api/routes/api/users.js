49a79772dad4e551f6ea57e6a2cee5a9b9580910
Bill <ceriustech@gmail.com>
2021-04-20 17:03:50 -0500

    Added JWT authentication for user registration.

--- api/routes/api/users.js
+++ api/routes/api/users.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/users');
@@ -61,6 +63,25 @@ router.post(
 
 			await user.save();
 
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) {
+						throw err;
+					} else {
+						res.json({ token });
+					}
+				}
+			);
+
 			// Return json web token
 		} catch (err) {
 			console.log(err.message);
@@ -68,7 +89,7 @@ router.post(
 		}
 
 		console.log(req.body);
-		res.send('USER Registered!');
+		res.send({ token });
 	}
 );
 
