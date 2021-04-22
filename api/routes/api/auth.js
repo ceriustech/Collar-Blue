93e9120a8d6e46f3489b17f420431599668326bc
Bill <ceriustech@gmail.com>
2021-04-21 23:50:18 -0500

    Created middleware that allows for the JSonwebtoken to be verified when it is transferred from the client so that users can be authenticated.

--- api/routes/api/auth.js
+++ api/routes/api/auth.js
@@ -1,14 +1,24 @@
 // Handles getting JWT for authentication
 
 // Handles creating auth
-
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/users');
 
 // @route GET api/posts
 // @desc Test route
 // @access Public
 
-router.get('/', (req, res) => res.send('AUTH ROUTE!'));
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
 
 module.exports = router;
