ef8620fbcdc26805d4f6ca88c5944b66d2952c95
Bill <ceriustech@gmail.com>
2021-04-23 18:16:53 -0500

    Created protected routes for authenticated users.

--- api/routes/api/auth.js
+++ api/routes/api/auth.js
@@ -3,7 +3,11 @@
 // Handles creating auth
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/users');
 
@@ -20,5 +24,72 @@ router.get('/', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+// @route POST api/auth
+// @desc Authenticate User and get JSON web token
+// @access Public
+
+router.post(
+	'/',
+	[
+		check('email', 'Please include a valid email.').isEmail(),
+		check('password', 'Password is required.').exists(),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+
+		try {
+			let user = await User.findOne({ email });
+
+			// Check if user exists
+			if (!user) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid user credentials.' }] });
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if (!isMatch) {
+				return res
+					.status(400)
+					.json({ errors: [{ msg: 'Invalid user credentials.' }] });
+			}
+
+			const payload = {
+				user: {
+					id: user.id,
+				},
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret'),
+				{ expiresIn: 360000 },
+				(err, token) => {
+					if (err) {
+						throw err;
+					} else {
+						res.json({ token });
+					}
+				}
+			);
+
+			// Return json web token
+		} catch (err) {
+			console.log(err.message);
+			res.status(500).send('Server Error!');
+		}
+
+		console.log(req.body);
+		res.send({ token });
+	}
+);
 
 module.exports = router;
