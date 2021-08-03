9ae26a2e3c54a3b1366a495f830b4925015f93c4
Bill <ceriustech@gmail.com>
2021-08-02 22:22:42 -0500

    Added a route to get a user profile by user id.

--- api/routes/api/profile.js
+++ api/routes/api/profile.js
@@ -161,4 +161,47 @@ router.get('/', async (req, res) => {
 	}
 });
 
+// @route  Get api/profiles/user/:user_id
+// @desc Get one user profile by user ID
+// @access Public
+
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id,
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile) return res.status(400).json({ msg: 'Profound not found' });
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind == 'ObjectId') {
+			return res.status(400).json({ msg: 'Profile not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
+
+// router.get(
+//   '/user/:user_id',
+//   checkObjectId('user_id'),
+//   async ({ params: { user_id } }, res) => {
+//     try {
+//       const profile = await Profile.findOne({
+//         user: user_id
+//       }).populate('user', ['name', 'avatar']);
+
+//       if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+//       return res.json(profile);
+//     } catch (err) {
+//       console.error(err.message);
+// 			if (err.kind == 'ObjectId') {
+// 				return res.status(400).json({ msg: 'Profile not found' });
+// 			}
+//       return res.status(500).json({ msg: 'Server error' });
+//     }
+//   }
+// );
+
 module.exports = router;
