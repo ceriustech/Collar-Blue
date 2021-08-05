489e2b404ac25a7fb10419cbbe0a79d24f8ca0ac
Bill <ceriustech@gmail.com>
2021-08-04 23:16:05 -0500

    Fixed a bug where the id property was not being read when deleting a profile or user.

--- api/routes/api/profile.js
+++ api/routes/api/profile.js
@@ -9,6 +9,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/users');
+
 // @route GET api/profile/me
 // @desc Get current users profile
 // @access Private
@@ -160,17 +161,15 @@ router.get('/user/:user_id', async (req, res) => {
 // @desc Delete profile, user, & posts
 // @access Public
 
-router.delete('/', async (req, res) => {
+router.delete('/', auth, async (req, res) => {
 	try {
-		// Remove posts
-
 		// Remove profile
 		await Profile.findOneAndRemove({ user: req.user.id });
 
 		// Remove user
 		await User.findOneAndRemove({ _id: req.user.id });
 
-		res.json({ msg: 'User has been deleted.' });
+		res.json({ msg: 'User deleted' });
 	} catch (err) {
 		console.error(err.message);
 		res.status(500).send('Server Error');
