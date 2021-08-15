7ea156cdcd19d3f82095c1c1e66a28e4ad429d33
Bill <ceriustech@gmail.com>
2021-08-15 18:26:50 -0500

    Added routes to find a single post and delete a single post.

--- api/routes/api/posts.js
+++ api/routes/api/posts.js
@@ -42,4 +42,73 @@ router.post(
 	}
 );
 
+// @route GET api/posts
+// @desc GET all posts
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+
+		res.json(posts);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Something went wrong: Server Error');
+	}
+});
+
+// @route GET api/post/:id
+// @desc GET post by ID
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		//Check Post
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+		res.json(post);
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		res.status(500).send('Something went wrong: Server Error');
+	}
+});
+
+// @route DELETE api/post/:id
+// @desc DELETE post by ID
+// @access Private
+
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		//Check Post
+		if (!post) {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		// Check User
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'User not authorized' });
+		}
+
+		await post.remove();
+
+		res.json({ msg: 'Post has been removed' });
+	} catch (err) {
+		console.error(err.message);
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({ msg: 'Post not found' });
+		}
+
+		res.status(500).send('Something went wrong: Server Error');
+	}
+});
+
 module.exports = router;
