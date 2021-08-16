3f9fbde286dca234d94ae22b9a31f3ea11a12f38
Bill <ceriustech@gmail.com>
2021-08-15 23:12:50 -0500

    Updated the route to like posts to a post request and added a put route to update the route.

--- api/routes/api/posts.js
+++ api/routes/api/posts.js
@@ -111,11 +111,11 @@ router.delete('/:id', auth, async (req, res) => {
 	}
 });
 
-// @route PUT api/post/like/:id
+// @route POST api/post/like/:id
 // @desc Like a  post
 // @access Private
 
-router.put('/like/:id', auth, async (req, res) => {
+router.post('/like/:id', auth, async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id);
 
@@ -128,6 +128,38 @@ router.put('/like/:id', auth, async (req, res) => {
 		}
 
 		post.likes.unshift({ user: req.user.id });
+
+		await post.save();
+
+		res.json(post.likes);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
+// @route PUT api/post/unlike/:id
+// @desc Unlike a  post
+// @access Private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Check if the post has already been liked
+		if (
+			post.likes.filter((like) => like.user.toString() === req.user.id)
+				.length === 0
+		) {
+			return res.status(400).json({ msg: 'Post has not been liked' });
+		}
+
+		// Get remove index
+		const removeIndex = post.likes
+			.map((like) => like.user.toString())
+			.indexOf(req.user.id);
+
+		post.likes.splice(removeIndex, 1);
 
 		await post.save();
 
