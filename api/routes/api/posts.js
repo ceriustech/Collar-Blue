75782b15166c7fe83c731b864cb4495245c58a70
Bill <ceriustech@gmail.com>
2021-08-16 23:09:58 -0500

    Added routes to add and delete user comments.

--- api/routes/api/posts.js
+++ api/routes/api/posts.js
@@ -170,4 +170,80 @@ router.put('/unlike/:id', auth, async (req, res) => {
 	}
 });
 
+// @route POST api/posts/comment/:id
+// @desc Comment on a post
+// @access Private
+
+router.post(
+	'/comment/:id',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			const user = await User.findById(req.user.id).select('-password');
+			const post = await Post.findById(req.params.id);
+
+			const newComment = {
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			};
+
+			post.comments.unshift(newComment);
+
+			await post.save();
+
+			res.json(post.comments);
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Something went wrong: Server Error');
+		}
+	}
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Delete comment
+// @access Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		// Find the a specific comment within the array of comments
+		const comment = post.comments.find(
+			(comment) => comment.id === req.params.comment_id
+		);
+		console.log(comment);
+
+		// Check if comment exists
+		if (!comment) {
+			return res.status(404).json({ msg: 'Comment does not exist' });
+		}
+
+		// Check that the user deleting the comment is the one that created it
+		if (comment.user.toString() !== req.user.id) {
+			return res.status(401).json({ msg: 'Not authorized' });
+		}
+
+		// Get the comment that will be removed
+		const removeIndex = post.comments
+			.map((comment) => comment.user.toString())
+			.indexOf(req.user.id);
+
+		post.comments.splice(removeIndex, 1);
+
+		await post.save();
+
+		res.json(post.comments);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Something went wrong: Server Error');
+	}
+});
+
 module.exports = router;
