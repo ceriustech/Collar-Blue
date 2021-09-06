b505585a8b6bdbf6377f9483730ba695ac3e1f68
Bill <ceriustech@gmail.com>
2021-09-05 22:57:27 -0500

    fixed an error with the package.json and added fontawesome to the app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './App.scss';
+
+library.add(fab, faCheckSquare, faCoffee);
 
 const App = () => {
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<p>This is a test</p>
+			<FontAwesomeIcon icon="check-square" />
+			<FontAwesomeIcon icon="fa-solid fa-truck-monster" />
+			<FontAwesomeIcon icon="fa-brands fa-twitter" />
+			<FontAwesomeIcon icon="fa-brands fa-font-awesome" />
+			<FontAwesomeIcon icon="fa-solid fa-truck-monster" />
 		</div>
 	);
 };
