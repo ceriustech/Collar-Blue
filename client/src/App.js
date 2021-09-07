f7a59ad114ce380161ca165f00c28095db80dbda
Bill <ceriustech@gmail.com>
2021-09-06 19:29:19 -0500

    fixed a bug with fontawesome not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './App.scss';
 
-library.add(fab, faCheckSquare, faCoffee);
+library.add(fab, fas);
 
 const App = () => {
 	return (
 		<div className="App">
 			<h1>Hello</h1>
 			<p>This is a test</p>
-			<FontAwesomeIcon icon="check-square" />
-			<FontAwesomeIcon icon="fa-solid fa-truck-monster" />
-			<FontAwesomeIcon icon="fa-brands fa-twitter" />
-			<FontAwesomeIcon icon="fa-brands fa-font-awesome" />
-			<FontAwesomeIcon icon="fa-solid fa-truck-monster" />
+			<Beverage />
+			<FontAwesomeIcon icon={['fas', 'truck-monster']} />
+			<FontAwesomeIcon icon={['fas', 'thumbs-up']} />
+			<FontAwesomeIcon icon={['fab', 'apple']} />
+			<FontAwesomeIcon icon={['fab', 'microsoft']} />
+			<FontAwesomeIcon icon={['fab', 'google']} />
 		</div>
 	);
 };
+
+const Beverage = () => (
+	<div>
+		<FontAwesomeIcon icon={['fas', 'check-square']} />
+		Your <FontAwesomeIcon icon={['fas', 'coffee']} /> is hot!
+	</div>
+);
 
 export default App;
